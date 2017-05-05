d2f74e45b5213a7ad0473a7d79f4b203e6b7c87c
Roman Shestakov <neoroma@gmail.com>
2017-05-05 16:21:59 +0300

    Fix formatting

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,35 +1,37 @@
+/* global React, ReactDOM  */
+
 const Title = React.createClass({
-    render() {
+  render() {
 
-        const { title, color = 'whitesmoke' } = this.props
+    const {title, color = 'whitesmoke'} = this.props
 
-        return (
-            React.DOM.div({},
-                React.DOM.h4({
-                    style: {
-                        color
-                    }
-                }, `TITLE PROP : ${title}`)
-            )
-        )
-    }
+    return (
+      React.DOM.div({},
+        React.DOM.h4({
+          style: {
+            color
+          }
+        }, `TITLE PROP : ${title}`)
+      )
+    )
+  }
 })
 
 const title = React.createFactory(Title)
 
 const Component = React.createClass({
-    render() {
-        return (
-            React.DOM.div({},
-                title({ title: 'prop 1', color: 'peru' }),
-                title({ title: 'prop 2', color: 'goldenrod' }),
-                title({ title: 'prop 3', color: 'tomato' }),
-                title({ title: 'prop 4', color: 'green' }),
-                title({ title: 'prop 5' }),
-                title({ title: 'this is cool' })
-            )
-        )
-    }
+  render() {
+    return (
+      React.DOM.div({},
+        title({title: 'prop 1', color: 'peru'}),
+        title({title: 'prop 2', color: 'goldenrod'}),
+        title({title: 'prop 3', color: 'tomato'}),
+        title({title: 'prop 4', color: 'green'}),
+        title({title: 'prop 5'}),
+        title({title: 'this is cool'})
+      )
+    )
+  }
 })
 
 ReactDOM.render(React.createElement(Component), document.getElementById('app'))
