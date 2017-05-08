73fb490cad3be6a1b33eaf7cc6fb98ea2a838493
Roman Shestakov <neoroma@gmail.com>
2017-05-08 11:05:26 +0300

    Add webpack config initials

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['js', 'json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+
+    ]
+  }
+
+
+}
