a0daa5f820d731c7f154d4c52dfac73e927fdd7c
Roman Shestakov <neoroma@gmail.com>
2017-05-11 12:40:58 +0300

    Update formatting for source files

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -5,18 +5,18 @@ import {Title} from './MyTitle'
 const title = React.createFactory(Title)
 
 const Component = React.createClass({
-  render () {
-    return (
-      React.DOM.div({},
-        title({title: 'prop 1', color: 'peru'}),
-        title({title: 'prop 2', color: 'goldenrod'}),
-        title({title: 'prop 3', color: 'tomato'}),
-        title({title: 'prop 4', color: 'green'}),
-        title({title: 'prop 5'}),
-        title({title: 'this is cool'})
-      )
-    )
-  }
+    render () {
+        return (
+            React.DOM.div({},
+                title({title: 'prop 1', color: 'peru'}),
+                title({title: 'prop 2', color: 'goldenrod'}),
+                title({title: 'prop 3', color: 'tomato'}),
+                title({title: 'prop 4', color: 'green'}),
+                title({title: 'prop 5'}),
+                title({title: 'this is cool'})
+            )
+        )
+    }
 })
 
 ReactDOM.render(React.createElement(Component), document.getElementById('app'))
--- js/MyTitle.js
+++ js/MyTitle.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 export const Title = React.createClass({
-  render () {
-    const {title, color = 'whitesmoke'} = this.props
+    render () {
+        const {title, color = 'whitesmoke'} = this.props
 
-    return (
-      React.DOM.div({},
-        React.DOM.h4({
-          style: {
-            color
-          }
-        }, `TITLE PROP : ${title}`)
-      )
-    )
-  }
+        return (
+            React.DOM.div({},
+                React.DOM.h4({
+                    style: {
+                        color
+                    }
+                }, `TITLE PROP : ${title}`)
+            )
+        )
+    }
 })
