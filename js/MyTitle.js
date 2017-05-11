d4842596fbcf46936ef84ea8a782144e44550a96
Roman Shestakov <neoroma@gmail.com>
2017-05-11 13:45:04 +0300

    Add react config to eslint and watch to npm scripts

--- js/MyTitle.js
+++ js/MyTitle.js
@@ -3,15 +3,16 @@ import React from 'react'
 export const Title = React.createClass({
     render () {
         const {title, color = 'whitesmoke'} = this.props
+        const style = {
+            color
+        }
 
         return (
-            React.DOM.div({},
-                React.DOM.h4({
-                    style: {
-                        color
-                    }
-                }, `TITLE PROP : ${title}`)
-            )
+            <div>
+                <h2 style={style}>
+                    <span>{title}</span>
+                </h2>
+            </div>
         )
     }
 })
