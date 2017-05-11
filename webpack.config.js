b5ff02cad8f62c46bac3a228123dd8715909c22a
Roman Shestakov <neoroma@gmail.com>
2017-05-11 12:45:07 +0300

    Set eslint rule for webconfig file

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,24 @@
+/* eslint-env node */
+
 const path = require('path')
 
 module.exports = {
-  context: __dirname,
-  entry: './js/ClientApp.js',
-  devtool: 'eval',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['js', 'json']
-  },
-  stats: {
-    colors: true,
-    reasons: true,
-    chunks: true
-  },
-  module: {
-    rules: [
-
-    ]
-  }
-
-
+    context: __dirname,
+    entry: './js/ClientApp.js',
+    devtool: 'eval',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['js', 'json']
+    },
+    stats: {
+        colors: true,
+        reasons: true,
+        chunks: true
+    },
+    module: {
+        rules: []
+    }
 }
