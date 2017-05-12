b499c7d1dd600532ff5edbfb230a21ec7a55651d
Roman Shestakov <neoroma@gmail.com>
2017-05-12 17:29:20 +0300

    Add history changes to webpack dev server config

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
         chunks: true
     },
     devServer: {
-        publicPath: '/public/'
+        publicPath: '/public/',
+        historyApiFallback: true
     },
     module: {
         rules: [
