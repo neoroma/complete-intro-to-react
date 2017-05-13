cca41261e437c93e5ed1a0d28a9ae929711ff492
Roman Shestakov <neoroma@gmail.com>
2017-05-13 23:57:06 +0300

    Add mapping the json data

--- js/Search.js
+++ js/Search.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import preload from '../public/data.json'
+import R from 'ramda'
+
+const getShows = R.prop('shows')
+const mapToTitle = R.prop('title')
+const toH3 = title => (<h3>{title}</h3>)
+const makeJsx = R.compose(toH3, mapToTitle)
+
+const workData = R.compose(R.map(makeJsx), getShows)
 
 export class Search extends React.Component {
     render() {
         return (
             <section className='search'>
-                <h2>This is a search page</h2>
+                {workData(preload)}
             </section>
         )
     }
