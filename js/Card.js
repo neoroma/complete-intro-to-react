416e393a843d972bfed369aeea16df6d816c4dc4
Roman Shestakov <neoroma@gmail.com>
2017-05-15 10:01:57 +0300

    Add prop types to make eslint happy

--- js/Card.js
+++ js/Card.js
@@ -1,24 +1,29 @@
 import React from 'react'
-import R from 'ramda'
-
-const getProps = R.pick(['title', 'poster', 'description', 'year'])
-
-const toMarkUp = ({title, poster, year, description}) => (
-    <div className='show-card'>
-        <img src={`/public/img/posters/${poster}`} />
-        <div>
-            <h3>{title}</h3>
-            <h4>({year})</h4>
-            <p>{description}</p>
-        </div>
-    </div>
-)
-
-const app = R.compose(toMarkUp, getProps)
+const {shape, string} = React.PropTypes
 
 export class Card extends React.Component {
+
+    static propTypes() {
+        return {
+            show: shape({
+                title: string,
+                poster: string,
+                year: string,
+                description: string,
+            })
+        }
+
+    }
+
     render() {
-        const {show} = this.props
-        return app(show)
+        const {show: {title, poster, year, description}} = this.props
+        return <div className='show-card'>
+            <img src={`/public/img/posters/${poster}`} />
+            <div>
+                <h3>{title}</h3>
+                <h4>({year})</h4>
+                <p>{description}</p>
+            </div>
+        </div>
     }
 }
--- js/MyTitle.js
+++ js/MyTitle.js
@@ -1,6 +1,15 @@
 import React from 'react'
+const {string} = React.PropTypes
 
 export const Title = React.createClass({
+
+    propTypes() {
+        return {
+            title: string,
+            color: string
+        }
+    },
+
     render () {
         const {title, color = 'whitesmoke'} = this.props
         const style = {
