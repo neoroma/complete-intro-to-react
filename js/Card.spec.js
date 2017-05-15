f58b5752cf9dfc2956ce112a9adcf626dc0b123e
Roman Shestakov <neoroma@gmail.com>
2017-05-15 23:10:27 +0300

    Add card test

---
+++ js/Card.spec.js
@@ -0,0 +1,12 @@
+/* global test, expect */
+
+import React from 'react'
+import {shallow} from 'enzyme'
+import {Card as ShowCard} from './Card'
+import {Search} from './Search'
+import preload from '../public/data.json'
+
+test('Amount of card should be the same as shows in the preload', () => {
+    const component = shallow(<Search />)
+    expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
--- js/Search.spec.js
+++ js/Search.spec.js
@@ -4,9 +4,21 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import {shallowToJson} from 'enzyme-to-json'
 import {Search} from './Search'
+import {Card as ShowCard} from './Card'
 
 test('Search component snapshot test', () => {
     const component = shallow(<Search />)
     const tree = shallowToJson(component)
     expect(tree).toMatchSnapshot()
+})
+
+test('Search should render correct amount of shows based on search', () => {
+    const search = 'house'
+    const component = shallow(<Search />)
+    component.find('input').simulate('change', {
+        target: {
+            value: search
+        }
+    })
+    expect(component.find(ShowCard).length).toEqual(2)
 })
