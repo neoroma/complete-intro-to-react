33deb5ed60bcd1acaf777ae306ec3b60dcc41f90
Roman Shestakov <neoroma@gmail.com>
2017-05-16 15:47:08 +0300

    Add details rendering some fields

--- js/Details.js
+++ js/Details.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import R from 'ramda'
 
 const getId = ({match: {params}}) => params.id
 
-export const render = (props) => (
-    <div className='details'>
-        <span>{getId(props)}</span>
-    </div>
-)
+export class Details extends React.Component {
+    render() {
 
+        const idParam = getId(this.props)
+        const {data: {shows}} = this.props
+        const finder = R.find(R.propEq('imdbID', idParam))
+        const show = finder(shows)
+
+        return (
+            <div className='details'>
+                <div>
+                    {show.title}
+                    {show.poster}
+                    {show.description}
+                </div>
+            </div>
+        )
+    }
+}
