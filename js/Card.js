c0f1cb3063074670a62bdbbbbe7d2f58be28bbec
Roman Shestakov <neoroma@gmail.com>
2017-05-21 23:51:55 +0300

    Add link to the card component

--- js/Card.js
+++ js/Card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {shape, string} from 'prop-types'
+import {Link} from 'react-router-dom'
 
 export class Card extends React.Component {
 
@@ -7,24 +8,27 @@ export class Card extends React.Component {
     static propTypes() {
         return {
             show: shape({
-                title: string,
-                poster: string,
-                year: string,
-                description: string,
+                title: string.isRequired,
+                poster: string.isRequired,
+                year: string.isRequired,
+                description: string.isRequired,
+                imdbID: string.isRequired
             })
         }
 
     }
 
     render() {
-        const {show: {title, poster, year, description}} = this.props
-        return <div className='show-card'>
-            <img src={`/public/img/posters/${poster}`} />
-            <div>
-                <h3>{title}</h3>
-                <h4>({year})</h4>
-                <p>{description}</p>
+        const {show: {title, poster, year, description, imdbID}} = this.props
+        return <Link to={`/details/${imdbID}`}>
+            <div className='show-card'>
+                <img src={`/public/img/posters/${poster}`} />
+                <div>
+                    <h3>{title}</h3>
+                    <h4>({year})</h4>
+                    <p>{description}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     }
 }
