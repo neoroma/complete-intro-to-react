ee7e6aac891baba69ee01ccf487669b43fa6f310
Roman Shestakov <neoroma@gmail.com>
2017-05-26 16:35:01 +0300

    Connect the redux extension for chrome

--- js/store.js
+++ js/store.js
@@ -1,4 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { rootReducer } from './reducers'
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, compose(
+    typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
+        ? window.devToolsExtension()
+        : f => f
+))
